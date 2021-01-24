e6d2304fa2e1e0b9d8562c279cc6d00c24869ad9
Felix Boudreau <felixboudreau16@gmail.com>
2021-01-23 20:18:50 -0500

    debut implémentation palette de couleur

---
+++ client/src/app/components/color/color.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-color',
+  templateUrl: './color.component.html',
+  styleUrls: ['./color.component.scss']
+})
+export class ColorComponent implements OnInit {
+  public hue: string
+  public color: string
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
