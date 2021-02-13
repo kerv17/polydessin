7eda5025ede590d156af9285b5a4746bd204a546
Kervin P <prinvillek@mail.com>
2021-02-12 19:31:04 -0500

    Added points to the junction points for line-service

---
+++ client/src/app/components/point-width-slider/point-width-slider/point-width-slider.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { MatSliderChange } from '@angular/material/slider';
+import { ToolControllerService } from '@app/services/tools/ToolController/tool-controller.service';
+
+@Component({
+    selector: 'app-point-width-slider',
+    templateUrl: './point-width-slider.component.html',
+    styleUrls: ['./point-width-slider.component.scss'],
+})
+export class PointWidthSliderComponent implements OnChanges {
+    @Input() pointWidth: number = 1;
+    @Input() change: boolean;
+    constructor(private tool: ToolControllerService) {}
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes.change) {
+            this.pointWidth = this.tool.currentTool.pointWidth;
+        }
+    }
+
+    getPointSliderValue(evt: MatSliderChange): void {
+        if (evt.value != null) {
+            this.tool.currentTool.pointWidth = evt.value;
+            this.pointWidth = this.tool.currentTool.pointWidth;
+        }
+    }
+}
--- client/src/app/components/sidebar/sidebar.component.ts
+++ client/src/app/components/sidebar/sidebar.component.ts
@@ -12,6 +12,8 @@ export class SidebarComponent {
     width: boolean = true;
     fillBorder: boolean = false;
     resetSlider: boolean = true;
+    linePoint: boolean = false;
+    lineOn: boolean = false;
     crayon: { backgroundColor: string } = { backgroundColor: 'white' };
     rectangle: { backgroundColor: string } = { backgroundColor: 'white' };
     line: { backgroundColor: string } = { backgroundColor: 'white' };
@@ -32,7 +34,8 @@ export class SidebarComponent {
 
     openLine(): void {
         this.service.setTool(Globals.lineShortcut);
-        this.openTool(false, true);
+        this.openTool(false, true, true);
+        this.linePoint = this.service.currentTool.toolMode === 'point';
         this.line = Globals.backgroundGainsoboro;
     }
 
@@ -41,10 +44,11 @@ export class SidebarComponent {
         this.openTool(true, true);
         this.ellipsis = Globals.backgroundGainsoboro;
     }
-    openTool(fillBorder: boolean, openWidth: boolean): void {
+    openTool(fillBorder: boolean, openWidth: boolean, lineOn: boolean = false): void {
         this.fillBorder = fillBorder;
         if (openWidth) this.openWidth();
         this.resetSlider = !this.resetSlider;
+        this.lineOn = lineOn;
         this.setButtonWhite();
     }
 
@@ -56,6 +60,11 @@ export class SidebarComponent {
     }
     setFillBorder(): void {
         this.service.setFillBorder();
+    }
+
+    setPoint(point: boolean): void {
+        this.service.currentTool.toolMode = point ? 'point' : 'noPoint';
+        this.linePoint = point;
     }
 
     openWidth(): void {
