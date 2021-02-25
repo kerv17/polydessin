04712531e8da1076c9b1a75fb452cf2439012c8d
Benjamin <benjamin.regazzoni@polymtl.ca>
2021-02-25 14:19:21 -0500

    debut implementation de la base de donnees

---
+++ server/app/classes/http.exceptions.ts
@@ -0,0 +1,11 @@
+import * as Httpstatus from "http-status-codes";
+
+export class HttpException extends Error {
+  constructor(
+    public status: number = Httpstatus.INTERNAL_SERVER_ERROR,
+    message: string
+  ) {
+    super(message);
+    this.name = "HttpException";
+  }
+}
