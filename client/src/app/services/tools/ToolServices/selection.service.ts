9140ae101a68687ccc24c99d5a707a98bc5a6f0e
Kervin P <prinvillek@mail.com>
2021-03-16 00:18:15 -0400

    Fixed selection issue with undoRedo Previewsly selected areas would reappear in the previewCtx

--- client/src/app/services/tools/ToolServices/selection.service.ts
+++ client/src/app/services/tools/ToolServices/selection.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Setting, Tool } from '@app/classes/tool';
+import { Tool } from '@app/classes/tool';
 import { Vec2 } from '@app/classes/vec2';
 import * as Globals from '@app/Constants/constants';
 import { DrawingService } from '@app/services/drawing/drawing.service';
@@ -19,18 +19,16 @@ export class SelectionService extends Tool {
 
     private firstCorner: Vec2;
     topLeftHandler: Vec2;
-    initialMousePosition: Vec2;
-    oppositeCorner: Vec2;
-    position: Vec2;
-    handlersPositions: Vec2[] = [];
-    initialSelectionPosition: Vec2;
+    private initialSelectionPosition: Vec2;
 
     constructor(drawingService: DrawingService, private selectionMove: SelectionMovementService) {
         super(drawingService);
         this.clearPath();
         this.width = 1;
+        this.topLeftHandler = { x: 0, y: 0 };
+        this.initialSelectionPosition = { x: 0, y: 0 };
+        this.selectedArea = { data: new Uint8ClampedArray([0, 0, 0, 0]), width: 1, height: 1 } as ImageData;
         this.rectangleService = new RectangleService(this.drawingService);
-
         document.addEventListener('keydown', (event: KeyboardEvent) => {
             this.selectionMove.onArrowKeyDown(event, this.inSelection);
             this.topLeftHandler = this.selectionMove.moveSelection(this.topLeftHandler);
@@ -69,13 +67,11 @@ export class SelectionService extends Tool {
                 this.updateCanvasOnMove(this.drawingService.previewCtx);
                 this.selectionMove.onMouseMove(event, this.drawingService.previewCtx, this.topLeftHandler, this.selectedArea);
             } else {
-this.pathData = this.rectangleService.getRectanglePoints(this.getPositionFromMouse(event));
-                this.oppositeCorner = this.pathData[2];
+                this.pathData = this.rectangleService.getRectanglePoints(this.getPositionFromMouse(event));
                 this.setTopLeftHandler();
                 this.drawBorder(this.drawingService.previewCtx);
                 this.selectArea(this.drawingService.baseCtx);
             }
-            // this.clearPath();
         }
     }
 
@@ -93,7 +89,6 @@ this.pathData = this.rectangleService.getRectanglePoints(this.getPositionFromMou
             }
         }
         this.mouseDown = false;
-        // this.clearPath();
     }
 
     onShift(shifted: boolean): void {
@@ -104,9 +99,7 @@ this.pathData = this.rectangleService.getRectanglePoints(this.getPositionFromMou
     onEscape(): void {
         if (this.inSelection) {
             this.confirmSelectionMove();
-            console.log(this.pathData);
             this.dispatchAction(this.createAction());
-
             this.inSelection = false;
             this.mouseDown = false;
             this.inMovement = false;
@@ -132,20 +125,24 @@ this.pathData = this.rectangleService.getRectanglePoints(this.getPositionFromMou
     private selectArea(ctx: CanvasRenderingContext2D): void {
         const width: number = this.pathData[2].x - this.pathData[0].x;
         const height: number = this.pathData[2].y - this.pathData[0].y;
-        this.selectedArea = ctx.getImageData(this.pathData[0].x, this.pathData[0].y, width, height);
+        if (width !== 0 && height !== 0) {
+            this.selectedArea = ctx.getImageData(this.pathData[0].x, this.pathData[0].y, width, height);
+        }
     }
 
     private updateCanvasOnMove(ctx: CanvasRenderingContext2D): void {
         ctx.fillStyle = 'white';
         ctx.strokeStyle = 'white';
-        ctx.fillRect(this.pathData[0].x, this.pathData[0].y, this.selectedArea.width, this.selectedArea.height);
+        ctx.fillRect(this.initialSelectionPosition.x, this.initialSelectionPosition.y, this.selectedArea.width, this.selectedArea.height);
         ctx.fillStyle = 'black';
         ctx.strokeStyle = 'black';
     }
 
     private confirmSelectionMove(): void {
         this.updateCanvasOnMove(this.drawingService.baseCtx);
-        this.drawingService.baseCtx.putImageData(this.selectedArea, this.pathData[4].x, this.pathData[4].y);
+        this.drawingService.baseCtx.putImageData(this.selectedArea, this.topLeftHandler.x, this.topLeftHandler.y);
+        this.drawingService.clearCanvas(this.drawingService.previewCtx);
+        this.pathData.push(this.topLeftHandler);
     }
 
     private drawBorder(ctx: CanvasRenderingContext2D): void {
@@ -182,10 +179,21 @@ this.pathData = this.rectangleService.getRectanglePoints(this.getPositionFromMou
     }
 
     doAction(action: DrawAction): void {
-      const previousSetting: Setting = this.saveSetting();
-      this.loadSetting(action.setting);
-      this.selectArea(this.drawingService.baseCtx);
-      this.confirmSelectionMove();
-      this.loadSetting(previousSetting);
-  }
+        const prevSetting = this.saveSetting();
+        const initialSelectionPosition = this.initialSelectionPosition;
+        const topLeftHandler = this.topLeftHandler;
+
+        this.loadSetting(action.setting);
+        this.selectArea(this.drawingService.baseCtx);
+        this.initialSelectionPosition = this.pathData[0];
+        // tslint:disable-next-line: no-magic-numbers
+        this.topLeftHandler = this.pathData[4];
+        this.confirmSelectionMove();
+
+        this.clearPreviewCtx();
+        this.loadSetting(prevSetting);
+        this.initialSelectionPosition = initialSelectionPosition;
+        this.topLeftHandler = topLeftHandler;
+        this.selectedArea = this.drawingService.baseCtx.getImageData(0, 0, 1, 1);
+    }
 }
