1c02c323eada57b1f45381e2066e0e4430f4b414
Nicholas Legrand <nicholas.legrand@polymtl.ca>
2021-04-04 01:37:33 -0400

    Ajout de la deuxieme fonctionnalité de remplissage

--- client/src/app/services/tools/ToolServices/bucket.service.ts
+++ client/src/app/services/tools/ToolServices/bucket.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Tool } from '@app/classes/tool';
 import { Vec2 } from '@app/classes/vec2';
+import * as Globals from '@app/Constants/constants';
 import { DrawingService } from '@app/services/drawing/drawing.service';
 @Injectable({
     providedIn: 'root',
@@ -32,7 +33,7 @@ export class BucketService extends Tool {
         }
 
         const color = this.getColorOfPixel(coords.x, coords.y, image);
-        console.log(color);
+
         this.pixelStack = new Array();
         this.pixelStack.push(coords);
         this.addPixelToStack(color, image);
@@ -98,26 +99,51 @@ export class BucketService extends Tool {
     }
 
     getColorOfPixel(x: number, y: number, image: ImageData): number[] {
-        const color: number[] = new Array(3);
+        const color: number[] = new Array(4);
 
         color[0] = image.data[(x + y * this.drawingService.canvas.width) * 4];
         color[1] = image.data[(x + y * this.drawingService.canvas.width) * 4 + 1];
         color[2] = image.data[(x + y * this.drawingService.canvas.width) * 4 + 2];
+        color[3] = image.data[(x + y * this.drawingService.canvas.width) * 4 + 3];
 
         return color;
     }
 
     shouldReplacePixel(coords: Vec2, color: number[], image: ImageData): boolean {
         const currentColor = this.getColorOfPixel(coords.x, coords.y, image);
+        let isAcceptable = true;
         for (let i = 0; i < currentColor.length; i++) {
-            if (this.isAcceptableValue(currentColor[i], color[i])) return true;
+            if (!this.isAcceptableValue(currentColor[i], color[i])) isAcceptable = false;
         }
-        console.log('tests');
-        return false;
+
+        return isAcceptable;
     }
 
     isAcceptableValue(current: number, reference: number): boolean {
         return Math.abs((current - reference) / 255) <= this.tolerance;
+    }
+
+    changeAllColors(event: MouseEvent): void {
+        const image: ImageData = this.drawingService.baseCtx.getImageData(0, 0, this.drawingService.canvas.width, this.drawingService.canvas.height);
+        const coords = this.getPositionFromMouse(event);
+        const color = this.getColorOfPixel(coords.x, coords.y, image);
+
+        for (let i = 0; i < image.data.length; i += Globals.PIXEL_SIZE) {
+            const currentCoords = {
+                x: (i / Globals.PIXEL_SIZE) % this.drawingService.baseCtx.canvas.width,
+                y: Math.trunc(i / Globals.PIXEL_SIZE / this.drawingService.baseCtx.canvas.width),
+            };
+
+            let isAcceptable = true;
+            for (let j = 0; j < color.length; j++) {
+                if (!this.isAcceptableValue(color[j], image.data[i + j])) {
+                    isAcceptable = false;
+                }
+            }
+            if (isAcceptable) {
+                this.drawingService.baseCtx.fillRect(currentCoords.x, currentCoords.y, 1, 1);
+            }
+        }
     }
 
     onMouseLeave(event: MouseEvent): void {
