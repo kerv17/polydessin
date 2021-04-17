9484bde45d2724824d84bef123c985f7f7a61482
Kervin Prinville <prinvillek@mail.com>
2021-04-17 16:38:26 -0400

    Added event to clipbaord to reset lasso

--- client/src/app/services/tools/ToolServices/clipboard/clipboard.service.ts
+++ client/src/app/services/tools/ToolServices/clipboard/clipboard.service.ts
@@ -55,6 +55,10 @@ export class ClipboardService extends Tool {
             // Il faut ajouter un customEvent pour faire changer le toolmode de lasso sans que lasso devienne le next tool.
             // Cet event devra être appelé ici si le toolMode est 'v'.
             // On fait passer le toolMode du lasso de movement à selection, sans que lasso devienne l'outil actif.
+            if(this.selection.toolMode === Globals.LASSO_SELECTION_SHORTCUT){
+                dispatchEvent(new CustomEvent('resetLassoToolMode'));
+            }
+            
             this.selection.toolMode = '';
             this.selection.selectedArea = new ImageData(this.clipboard.data, this.clipboard.width, this.clipboard.height);
             this.selection.drawingService.previewCtx.putImageData(this.clipboard, 0, 0);
